6c35f9b8090c3b21a8025cb37365ebdef262d7da
JenMai <jm.jennimai@gmail.com>
2019-01-25 10:51:03 +0100

    question 8-a

---
+++ src/Controller/MatchController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Game;
+use App\Form\GameType;
+use App\Repository\GameRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/match")
+ */
+class MatchController extends AbstractController
+{
+    /**
+     * @Route("/", name="match_index", methods={"GET"})
+     */
+    public function index(GameRepository $gameRepository): Response
+    {
+        return $this->render('match/index.html.twig', [
+            'games' => $gameRepository->findAll(),
+        ]);
+    }
+}
