96d99d7c5dbd8df5226f2435e4fe69efb1ee5e2c
JenMai <jm.jennimai@gmail.com>
2019-01-25 09:30:20 +0100

    question 5

--- src/Form/TeamType.php
+++ src/Form/TeamType.php
@@ -6,14 +6,19 @@ use App\Entity\Team;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class TeamType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('flag')
+            ->add('teams', EntityType::class, [
+                'class' => Team::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('t')
+                        ->orderBy('t.name', 'DESC');
+                },
         ;
     }
 
