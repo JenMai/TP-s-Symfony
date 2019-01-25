3f3b114e1f2409035b5c09f3a8e0825c91e33709
JenMai <jm.jennimai@gmail.com>
2019-01-25 09:43:15 +0100

    fix fix question 5

--- src/Form/Type/TeamTypeType.php
+++ src/Form/Type/TeamTypeType.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Form;
+namespace App\Form\Type;
 
 use App\Entity\Team;
 use Symfony\Component\Form\AbstractType;
@@ -8,13 +8,18 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
-class TeamType extends AbstractType
+class TeamTypeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->('tag')
+            ->add('teams', EntityType::class, [
+                'class' => Team::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('t')
+                        ->orderBy('t.name', 'DESC');
+                },
+                'choice_label' => 'name',
         ;
     }
 
