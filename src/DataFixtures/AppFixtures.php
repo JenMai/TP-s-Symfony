395c23d9c710e3306df9f55aa073a793ff433cbb
JenMai <jm.jennimai@gmail.com>
2019-03-28 09:59:18 +0100

    question 1.1

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,10 +2,9 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Game;
-use App\Entity\Team;
 use App\Entity\User;
-use App\Entity\Weapon;
+use App\Entity\Product;
+use App\Entity\UserProduct;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -18,6 +17,41 @@ class AppFixtures extends Fixture {
 
     public function load(ObjectManager $manager) {
 
+        $product1 = new Product();
+        $product1->setName("Produit1");
+        $product1->setPrice(11.50);
+        $product1->setQuantity(10);
+        $product1->setCreatedAt(new \DateTime('now'));
+        $manager->persist($product1);
+
+        $product2 = new Product();
+        $product2->setName("Produit2");
+        $product2->setPrice(11.50);
+        $product2->setQuantity(10);
+        $product2->setCreatedAt(new \DateTime('now'));
+        $manager->persist($product2);
+
+        $product3 = new Product();
+        $product3->setName("Produit3");
+        $product3->setPrice(11.50);
+        $product3->setQuantity(10);
+        $product3->setCreatedAt(new \DateTime('now'));
+        $manager->persist($product3);
+
+        $product4 = new Product();
+        $product4->setName("Produit4");
+        $product4->setPrice(11.50);
+        $product4->setQuantity(10);
+        $product4->setCreatedAt(new \DateTime('now'));
+        $manager->persist($product4);
+
+        $product5 = new Product();
+        $product5->setName("Produit5");
+        $product5->setPrice(11.50);
+        $product5->setQuantity(10);
+        $product5->setCreatedAt(new \DateTime('now'));
+        $manager->persist($product5);
+
         $user1 = new User();
         $user1->setEnabled(true);
         $user1->setEmail('user@user.fr');
@@ -25,6 +59,20 @@ class AppFixtures extends Fixture {
         $user1->setLastName('userlast');
         $user1->setPassword($this->passwordEncoder->encodePassword($user1,'user@user.fr'));
         $manager->persist($user1);
+
+        $userProduct = new UserProduct();
+        $userProduct->setCreatedAt(new \DateTime('now'));
+        $userProduct->setProduct($product1);
+        $userProduct->setQuantity(1);
+        $userProduct->setUser($user1);
+        $manager->persist($userProduct);
+
+        $userProduct = new UserProduct();
+        $userProduct->setCreatedAt(new \DateTime('now'));
+        $userProduct->setProduct($product2);
+        $userProduct->setQuantity(2);
+        $userProduct->setUser($user1);
+        $manager->persist($userProduct);
 
         $user1 = new User();
         $user1->setEnabled(true);
@@ -34,6 +82,20 @@ class AppFixtures extends Fixture {
         $user1->setPassword($this->passwordEncoder->encodePassword($user1,'user1@user.fr'));
         $manager->persist($user1);
 
+        $userProduct = new UserProduct();
+        $userProduct->setCreatedAt(new \DateTime('now'));
+        $userProduct->setProduct($product3);
+        $userProduct->setQuantity(2);
+        $userProduct->setUser($user1);
+        $manager->persist($userProduct);
+
+        $userProduct = new UserProduct();
+        $userProduct->setCreatedAt(new \DateTime('now'));
+        $userProduct->setProduct($product4);
+        $userProduct->setQuantity(4);
+        $userProduct->setUser($user1);
+        $manager->persist($userProduct);
+
         $user1 = new User();
         $user1->setEnabled(true);
         $user1->setEmail('user2@user.fr');
@@ -42,6 +104,19 @@ class AppFixtures extends Fixture {
         $user1->setPassword($this->passwordEncoder->encodePassword($user1,'user2@user.fr'));
         $manager->persist($user1);
 
+        $userProduct = new UserProduct();
+        $userProduct->setCreatedAt(new \DateTime('now'));
+        $userProduct->setProduct($product5);
+        $userProduct->setQuantity(2);
+        $userProduct->setUser($user1);
+        $manager->persist($userProduct);
+
+        $userProduct = new UserProduct();
+        $userProduct->setCreatedAt(new \DateTime('now'));
+        $userProduct->setProduct($product4);
+        $userProduct->setQuantity(1);
+        $userProduct->setUser($user1);
+        $manager->persist($userProduct);
 
         $manager->flush();
     }
