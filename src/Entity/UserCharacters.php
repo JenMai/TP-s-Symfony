dc2446d29edd6efeab8bb4176096060923a2e932
Maillot Jennifer <jennifer_maillot@ens.univ-artois.fr>
2019-03-29 21:07:33 +0700

    question 3.2

--- src/Entity/UserCharacters.php
+++ src/Entity/UserCharacters.php
@@ -4,8 +4,11 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserCharactersRepository")
+ * @UniqueEntity({"user", "characters"})
  */
 class UserCharacters
 {
