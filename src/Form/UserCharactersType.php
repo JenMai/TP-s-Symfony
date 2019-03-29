57ec19e733db47d21941699de97902b94c06ee9b
Maillot Jennifer <jennifer_maillot@ens.univ-artois.fr>
2019-03-29 20:51:26 +0700

    question 3.1 + highlight avancement

--- src/Form/UserCharactersType.php
+++ src/Form/UserCharactersType.php
@@ -7,8 +7,23 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+
 class UserCharactersType extends AbstractType
 {
+    private $securityChecker;
+    private $token;
+
+    public function __construct(AuthorizationCheckerInterface $securityChecker, TokenStorageInterface $token)
+    {
+        $this->securityChecker = $securityChecker;
+        $this->token = $token;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -18,6 +33,32 @@ class UserCharactersType extends AbstractType
             ->add('user')
             ->add('characters')
         ;
+
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            array($this, 'preSetData')
+        );
+    }
+
+    public function preSetData(FormEvent $event)
+    {
+        $form = $event->getForm();
+        $userCharacter = $event->getData();
+
+        if($this->securityChecker->isGranted('ROLE_ADMIN') === false){
+            $form->remove('user');
+            $form->remove('default');
+            $form->remove('favorite');
+            $form->remove('createdAt');
+
+            $userCharacter->setUser($this->token->getToken()->getUser());
+            $userCharacter->setDefaultCharacter(false);
+            $userCharacter->setFavorite(false);
+            $userCharacter->setCreatedAt(new \DateTime('now'));
+
+        }
+
+        
     }
 
     public function configureOptions(OptionsResolver $resolver)
